7e8967da875e3248576ea293081e4dd90d8b6332
Diego Gardunño <al222010696@gmail.com>
2023-10-07 20:30:01 -0600

    avanse vista usuarrios

---
+++ resources/views/modulousuarios.blade.php
@@ -0,0 +1,31 @@
+<link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script
+
+<h1>Modulo Usuarios</h1>
+
+<table class="table">
+  <thead>
+    <tr>
+      
+    <th>ID</th>
+      <th>Nombre</th>
+      <th>Telefono</th>
+      <th>Correo Istitucional </th>
+      <th>Matricula </th>
+      <th>Rol</th>
+    </tr>
+  </thead>
+  <tbody>
+    
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    
+  </tbody>
+</table>
