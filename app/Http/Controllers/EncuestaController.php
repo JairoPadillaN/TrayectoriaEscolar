6c656726f004a7680d7ee3bde0b63247f13f689a
GonzalezJandro <71019767+GonzalezJandro@users.noreply.github.com>
2023-11-14 11:04:04 -0600

    (add) Tabs y preguntas
    Se inicio con el modulo de la encuesta, se agregaron los tabs para las categorias y preguntas por categoria

--- app/Http/Controllers/EncuestaController.php
+++ app/Http/Controllers/EncuestaController.php
@@ -12,9 +12,16 @@ class EncuestaController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
-        $preguntas = Preguntas::with('categoria')->get();
-        $categorias = Categorias::all();
-        return view('encuesta.index', compact('preguntas','categorias'));
-    }
+{
+    $preguntas = Preguntas::with('categoria')->get();
+    $categorias = Categorias::all();
+    return view('encuesta.index', compact('preguntas', 'categorias'));
+}
+
+public function showTabs()
+{
+    $categorias = Categorias::all();
+    return view('encuesta.tabs', compact('categorias'));
+}
+
 }
--- resources/views/encuesta/index.blade.php
+++ resources/views/encuesta/index.blade.php
@@ -1,38 +1,42 @@
-@extends('encuesta')
-@section('encuestaList')
-<div class="row" style="padding-top: 70px">
-  <div class="col-md-2"></div>
-  <div class="col-md-8">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Encuesta</h2>
+        <div class="row" style="padding-top: 70px">
+            <div class="col-md-2"></div> <div class="col-md-8"> <div class="container"> 
+                <ul class="nav nav-tabs">
+                @foreach($categorias as $categoria) 
+                <li class="nav-item"> 
+                    <a class="nav-link" href="#tab-{{ $categoria->id }}" data-toggle="tab">{{ $categoria->categoria }}</a> 
+                </li>
+                @endforeach
+                </ul>
 
-  <div class="container">
-    <ul class="nav nav-tabs" id="myTab" role="tablist">
-        <li class="nav-item" role="presentation">
-            <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Datos Generales</button>
-        </li>
-        <li class="nav-item" role="presentation">
-            <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Domicilio de residencia</button>
-        </li>
-        <li class="nav-item" role="presentation">
-            <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Datos de contacto de Emergencia</button>
-        </li>
-    </ul>
-    <div class="tab-content" id="myTabContent">
-        
-        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-            <p>@include('encuesta.modules.datos_generales')</p>
-        </div>
+                <div class="tab-content"> @foreach($categorias as $categoria) 
+                    <div class="tab-pane" id="tab-{{ $categoria->id }}">
+                    <h4>{{ $categoria->categoria }}</h4>
 
-        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-            <p>@include('encuesta.modules.domicilio_residencia')</p>
-        </div>
-        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-            <p>@include('encuesta.modules.datos_contacto_emergemcia')</p>
+                    @foreach($preguntas as $pregunta)
+                        @if($pregunta->categoria->id == $categoria->id)
+                        <p>{{ $pregunta->pregunta }}</p>
+                        @endif
+                    @endforeach
+                    </div>
+                    @endforeach
+
+            </div>
+            </div>
         </div>
     </div>
-</div>
-
-
-</div>
-  <div class="col-md-2"></div>
-</div>
-@endsection
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
